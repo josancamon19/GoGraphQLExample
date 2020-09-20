7144324f372df8b31e46314bdc8465f957b6defd
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2020-09-20 18:40:04 -0500

    GraphQL tutorials edge part 2

--- main.go
+++ main.go
@@ -8,148 +8,137 @@ import (
 	"log"
 )
 
-func main() {
-	var commentType = graphql.NewObject(graphql.ObjectConfig{
-		Name: "Comment",
-		Fields: graphql.Fields{
-			"Body": &graphql.Field{
-				Type: graphql.String,
-			},
-		},
-	})
-	var authorType = graphql.NewObject(graphql.ObjectConfig{
-		Name: "Author",
-		Fields: graphql.Fields{
-			"Name": &graphql.Field{
-				Type: graphql.String,
-			},
-			"Tutorials": &graphql.Field{
-				Type: graphql.NewList(graphql.Int),
-			},
-		},
-	})
+var tutorials []models.Tutorial
 
-	var tutorialType = graphql.NewObject(graphql.ObjectConfig{
-		Name: "Tutorial", Fields: graphql.Fields{
-			"ID": &graphql.Field{
+var commentType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Comment",
+	Fields: graphql.Fields{
+		"Body": &graphql.Field{
+			Type: graphql.String,
+		},
+	},
+})
+var authorType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Author",
+	Fields: graphql.Fields{
+		"Name": &graphql.Field{
+			Type: graphql.String,
+		},
+		"Tutorials": &graphql.Field{
+			Type: graphql.NewList(graphql.Int),
+		},
+	},
+})
+
+var tutorialType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Tutorial", Fields: graphql.Fields{
+		"ID": &graphql.Field{
+			Type: graphql.Int,
+		},
+		"Title": &graphql.Field{
+			Type: graphql.String,
+		},
+		"Author": &graphql.Field{
+			Type: authorType,
+		},
+		"Comments": &graphql.Field{
+			Type: graphql.NewList(commentType),
+		},
+	},
+})
+
+var fields = graphql.Fields{
+	"tutorial": &graphql.Field{
+		Type:        tutorialType,
+		Description: "Get tutorial by ID",
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
 				Type: graphql.Int,
 			},
-			"Title": &graphql.Field{
-				Type: graphql.String,
-			},
-			"Author": &graphql.Field{
-				Type: authorType,
-			},
-			"Comments": &graphql.Field{
-				Type: graphql.NewList(commentType),
-			},
 		},
-	})
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			id, ok := p.Args["id"].(int)
+			if ok {
+				tutorialsFakeData := models.Populate()
+				for _, tutorial := range tutorialsFakeData {
+					if tutorial.ID == id {
+						return tutorial, nil
+					}
+				}
+			}
+			return nil, nil
+		},
+	},
+	"list": &graphql.Field{
+		Type:        graphql.NewList(tutorialType),
+		Description: "Get tutorial list",
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			return models.Populate(), nil
+		},
+	},
+}
 
-	fields := graphql.Fields{
-		"tutorial": &graphql.Field{
+var mutationType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"create": &graphql.Field{
+			Description: "Create a new tutorial",
 			Type:        tutorialType,
-			Description: "Get tutorial by ID",
 			Args: graphql.FieldConfigArgument{
-				"id": &graphql.ArgumentConfig{
-					Type: graphql.Int,
+				"Title": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
 				},
 			},
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				id, ok := p.Args["id"].(int)
-				if ok {
-					tutorialsFakeData := models.Populate()
-					for _, tutorial := range tutorialsFakeData {
-						if tutorial.ID == id {
-							return tutorial, nil
-						}
-					}
+				tutorial := models.Tutorial{
+					Title: p.Args["Title"].(string),
 				}
-				return nil, nil
+				tutorials = append(tutorials, tutorial)
+				return tutorial, nil
 			},
 		},
-		"list": &graphql.Field{
-			Type:        graphql.NewList(tutorialType),
-			Description: "Get tutorial list",
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return models.Populate(), nil
-			},
-		},
-	}
+	},
+})
+
+func main() {
+	// TODO include database with gorm ORM package
+	tutorials = models.Populate()
 	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schemaConfig := graphql.SchemaConfig{
+		Query:    graphql.NewObject(rootQuery),
+		Mutation: mutationType,
+	}
 	schema, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
 		log.Fatalf("failed to create new schema, error: %v", err)
 	}
 
-	// Doing query
-//	query := `
-//    {
-//        list {
-//            ID
-//            Title
-//            Comments {
-//                Body
-//            }
-//            Author {
-//                Name
-//                Tutorials
-//            }
-//        }
-//    }
-//`
+	// Query
 	query := `
-    {
-        tutorial(id:1) {
+    mutation {
+        create(Title: "Hello World") {
             Title
-            Author {
-                Name
-                Tutorials
-            }
         }
     }
-`
-	params := graphql.Params{Schema: schema, RequestString: query}
-	response := graphql.Do(params)
-	if len(response.Errors) > 0 {
-		log.Fatalf("failed to execute graphql operation, errors: %+v", response.Errors)
-	}
-
-	rJSON, _ := json.Marshal(response)
-	fmt.Printf("%s \n", rJSON)
-}
-
-func SimpleExample() {
-	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world", nil
-			},
-			Description: "Test field",
-		},
-	}
-
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		log.Fatalf("failed to create new schema, error: %v", err)
-	}
-
-	// Doing query
-	query := `
-		{
-			hello
-		}
 	`
-	params := graphql.Params{Schema: schema, RequestString: query}
-	response := graphql.Do(params)
-	if len(response.Errors) > 0 {
-		log.Fatalf("failed to execute graphql operation, errors: %+v", response.Errors)
+	applyQuery(schema, query)
+	query = `
+	{
+		list {
+			ID
+			Title
+		}
 	}
+	`
+	applyQuery(schema, query)
 
-	rJSON, _ := json.Marshal(response)
-	fmt.Printf("%s \n", rJSON) // {“data”:{“hello”:”world”}}
+}
+func applyQuery(schema graphql.Schema, query string) {
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
 }
